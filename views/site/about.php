2b96854ddd133fca2ddd7bed69b275970a2137f3
johairamae <johairamae@gmail.com>
2017-05-23 10:48:44 +0800

    first commit for online ferd project monitoring

---
+++ views/site/about.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+	<div class="jumbotron">
+        
+		<img src="/pics/ferd.png" alt="erdb logo" style="width:750px;height:210px;">
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+    <h4>
+       The FOREST ECOSYSTEMS RESEARCH DIVISION (FERD) plans, formulates, coordinates and monitors RDE PAPs related to the conservation and sustainable management of production and protection forests, upland farms, and grasslands, including rehabilitation of mining and degraded areas. It conducts basic researches and develops standards and protocols on said areas of concerns and assists in the technology assessment, integration and packaging. FERD also develops, improves, and recommend policies and guidelines for sustainable management of forest ecosystem.
+    </h4>
+
+    
+</div>
